90a940fe371d832ad0373e478aa7673e9b3b911c
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-18 20:41:43 -0500

    Ticketing-app

--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -1,15 +1,30 @@
 package com.example.service.impl;
 
 import com.example.dto.UserDTO;
+import com.example.entity.User;
+import com.example.repository.UserRepository;
 import com.example.service.UserService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<UserDTO> listAllUsers() {
+
+        List<User> userList = userRepository.findAll(Sort.by("firstName"));
+
+
+
         return null;
     }
 
