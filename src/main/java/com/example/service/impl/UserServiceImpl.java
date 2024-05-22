e30754b5e3553b942c8ee3ac25b40028204de8a1
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-22 16:35:10 -0500

    Ticketing-app

--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -64,4 +64,13 @@ public class UserServiceImpl implements UserService {
 
     }
 
+    @Override
+    public void delete(String username) {
+
+        User user = userRepository.findByUserName(username);
+        user.setIsDeleted(true);
+        userRepository.save(user);
+
+    }
+
 }
