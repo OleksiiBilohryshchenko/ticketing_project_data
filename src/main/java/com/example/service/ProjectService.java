0d96b8a05392c86004a0aa99b75d7b08b64c80a8
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-23 12:01:25 -0500

    Ticketing-app

---
+++ src/main/java/com/example/service/ProjectService.java
@@ -0,0 +1,15 @@
+package com.example.service;
+
+import com.example.dto.ProjectDTO;
+
+import java.util.List;
+
+public interface ProjectService {
+
+    ProjectDTO getByProjectCode(String code);
+    List<ProjectDTO> listAllProjects();
+    void save(ProjectDTO dto);
+    void update(ProjectDTO dto);
+    void delete(String code);
+
+}
