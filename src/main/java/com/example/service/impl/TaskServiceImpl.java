098de81ad990db2fa30787e98abd6a1b9b41e011
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-05-24 16:12:23 -0500

    Ticketing-app

--- src/main/java/com/example/service/impl/TaskServiceImpl.java
+++ src/main/java/com/example/service/impl/TaskServiceImpl.java
@@ -1,12 +1,17 @@
 package com.example.service.impl;
 
 import com.example.dto.TaskDTO;
+import com.example.entity.Task;
+import com.example.entity.User;
+import com.example.enums.Status;
 import com.example.mapper.TaskMapper;
 import com.example.repository.TaskRepository;
 import com.example.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,20 +33,46 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public void save(TaskDTO dto) {
 
+        dto.setTaskStatus((Status.OPEN));
+        dto.setAssignedDate(LocalDate.now());
+        Task task = taskMapper.convertToEntity((dto));
+        taskRepository.save(task);
     }
 
     @Override
     public void update(TaskDTO dto) {
+
+        Optional<Task> task = taskRepository.findById((dto.getId()));
+        Task convertedTask = taskMapper.convertToEntity(dto);
+
+        if (task.isPresent()){
+            convertedTask.setTaskStatus(task.get().getTaskStatus());
+            convertedTask.setAssignedDate(task.get().getAssignedDate());
+            taskRepository.save(convertedTask);
+        }
 
     }
 
     @Override
     public void delete(Long id) {
 
+        Optional<Task> foundTask = taskRepository.findById(id);
+
+        if(foundTask.isPresent()){
+            foundTask.get().setIsDeleted(true);
+            taskRepository.save(foundTask.get());
+        }
     }
 
     @Override
     public TaskDTO findById(Long id) {
+
+        Optional<Task> task = taskRepository.findById(id);
+
+        if (task.isPresent()){
+            return taskMapper.convertToDTO(task.get());
+        }
+
         return null;
     }
 }
