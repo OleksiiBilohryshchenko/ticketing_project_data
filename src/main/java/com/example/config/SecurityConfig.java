4ace1e01a72a35000a7eb41c7f6b2b330ee085f0
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-06-06 17:03:02 -0500

    Ticketing-app

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 import java.util.ArrayList;
@@ -33,5 +35,32 @@ public class SecurityConfig {
 
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/user/**").hasRole("ADMIN")
+                        .requestMatchers("/project/**").hasRole("MANAGER")
+                        .requestMatchers("/task/employee/**").hasRole("EMPLOYEE")
+                        .requestMatchers("/task/**").hasRole("MANAGER")
+//                        .requestMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
+                        .requestMatchers(
+                                "/",
+                                "/login",
+                                "/fragments/**",
+                                "/assets/**",
+                                "/images/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin( form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/welcome")
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                );
+        return http.build();
 
+
+    }
 }
