ebb4ea471d24522ab391434f5071b59a5375a64e
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-06-06 11:58:03 -0500

    Ticketing-app

---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        List<UserDetails> userList = new ArrayList<>();
+
+        userList.add(
+                new User("Mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")));
+
+        )
+
+    }
+
+
+}
